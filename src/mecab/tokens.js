22e20dea52b448186289f3875d6bc289ff383a94
dehmer <horst.dehmer@syncpoint.io>
2025-01-31 10:02:36 +0100

    optimized mecab analysis.

--- src/mecab/tokens.js
+++
@@ -1,6 +0,0 @@
-const { tokenizeSync } = require("@enjoyjs/node-mecab")
-
-const tokens = s => tokenizeSync(s)
-	.filter(tokens => !['BOS', 'EOS'].includes(tokens.surface))
-
-module.exports = tokens
